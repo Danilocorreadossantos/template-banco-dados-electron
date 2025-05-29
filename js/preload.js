24b3775cc63f5582fefc9b50ccb2c9e705b5822f
Danilo Correa <danilocorrea.santos@hotmail.com>
2025-05-28 21:41:25 -0300

    Banco de dados adicionado

---
+++ js/preload.js
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer} = require("electron")
+
+contextBridge.exposeInMainWorld("api", {
+    cadastrarUsuario: (campoNome, campoEmail) => {
+        console.log("cadastrar usuario")
+
+        ipcRenderer.invoke(criar-usuario,campoNome,campoEmail)
+    }
+})
--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const mysql = require("mysql2/promise") // Promise 
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload : "./preload.js",
+      contextIsolation: true,
+    }
   })
 
   win.loadFile('pages/index.html')
 }
+
+async function conectarBancoDados() {
+  var conexao = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "electron_db"
+  })
+  return conexao
+}
+
+ipcMain.handle("criar-usuario", async function (evento, campoNome, campoEmail) {
+  var conexao = await conectarBancoDados()
+
+  var criarUsuarioSQL = "INSERT INTO usuarios(nome, email) VALUES(?, ?)"
+
+  var resultado = await conexao.execute(criarUsuarioSQL, [campoNome, campoEmail])
+
+  console.logo("resultado", resultado)
+})
 
 app.whenReady().then(() => {
   createWindow()
